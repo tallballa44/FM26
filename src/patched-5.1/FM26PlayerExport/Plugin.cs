330ca65335769bcac7f08a4967bf0471b3f4b2c2
tallballa44 <86196798+tallballa44@users.noreply.github.com>
2026-09-05 10:46:46 -0700

    feat: identify Patch 0.1 build

---
+++ src/patched-5.1/FM26PlayerExport/Plugin.cs
@@ -0,0 +1,30 @@
+using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using UnityEngine;
+
+namespace FM26PlayerExport;
+
+[BepInPlugin("com.koda.fm26.playerexport", "FM26 Player Export", "5.1.1")]
+public class Plugin : BasePlugin
+{
+    internal static new ManualLogSource Log;
+    private static ExportBehaviour _behaviour;
+
+    public override void Load()
+    {
+        Log = base.Log;
+        Log.LogInfo("[FM26Export Patch 0.1] Loaded. Hotkeys: F9 or Ctrl+P = export | F8 = UI diagnostics");
+        PluginConfig.Init(base.Config, Log);
+        _behaviour = AddComponent<ExportBehaviour>();
+    }
+
+    public override bool Unload()
+    {
+        Log.LogInfo("[FM26Export Patch 0.1] Unloading plugin...");
+        if (_behaviour != null)
+            Object.Destroy(_behaviour);
+
+        return base.Unload();
+    }
+}
