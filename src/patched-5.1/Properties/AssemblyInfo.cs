f1b8624832d0649eddcbe1ba374f3b2ccb4b6eb6
tallballa44 <86196798+tallballa44@users.noreply.github.com>
2026-09-05 10:47:12 -0700

    build: version Patch 0.1 as 5.1.1

---
+++ src/patched-5.1/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+
+[assembly: AssemblyCompany("FM26PlayerExport")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyFileVersion("5.1.1.0")]
+[assembly: AssemblyInformationalVersion("5.1.1-patch0.1")]
+[assembly: AssemblyProduct("FM26PlayerExport")]
+[assembly: AssemblyTitle("FM26PlayerExport")]
+[assembly: AssemblyVersion("5.1.1.0")]
