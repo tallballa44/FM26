0a30fbc371c3d28fac0840f92429ccea18c9d3fd
tallballa44 <86196798+tallballa44@users.noreply.github.com>
2026-09-05 20:42:33 -0700

    build: set Patch 0.5 assembly version

--- src/patched-5.1/Properties/AssemblyInfo.cs
+++ src/patched-5.1/Properties/AssemblyInfo.cs
@@ -2,8 +2,8 @@ using System.Reflection;
 
 [assembly: AssemblyCompany("FM26PlayerExport")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyFileVersion("5.1.4.0")]
-[assembly: AssemblyInformationalVersion("5.1.4-patch0.4")]
+[assembly: AssemblyFileVersion("5.1.5.0")]
+[assembly: AssemblyInformationalVersion("5.1.5-patch0.5")]
 [assembly: AssemblyProduct("FM26PlayerExport")]
 [assembly: AssemblyTitle("FM26PlayerExport")]
-[assembly: AssemblyVersion("5.1.4.0")]
+[assembly: AssemblyVersion("5.1.5.0")]
